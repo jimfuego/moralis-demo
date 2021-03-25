c1454120c6e77961a57079cc29447e9208eb277c
jimmyb <jimmyb89@yahoo.com>
2021-03-25 18:37:47 -0500

    add home form

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,11 +3,21 @@ import React, { useState, useEffect } from 'react';
 
 const Home = (props) => {
 
-    return(
-      <div>
-        Home Page
+  const submit = () => {
+    console.log('submitted');
+  }
+
+  return (
+    <div>
+      <div className="input-group mb-3 row">
+        <span className="input-group-text" id="basic-addon1">@</span>
+        <input type="text" className="form-control" placeholder="Wallet Address" aria-label="Wallet Address" aria-describedby="basic-addon1" />
       </div>
-    )
+      <div className='row'>
+        <button className="btn btn-primary" type="submit" onClick={() => submit()}>Button</button>
+      </div>
+    </div>
+  )
 
 }
 
